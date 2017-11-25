9dd8390fc02573f1462dc11772ed267871847e78
kvswim <kvswim@gmail.com>
2017-11-24 18:15:13 -0600

    NN prototype

--- HW3/p1a.py
+++ HW3/p1a.py
@@ -16,18 +16,21 @@ import numpy as np
 import torch.nn as nn
 from torch.autograd import Variable
 from optparse import OptionParser
+from PIL import Image
 
-#command line switches, supports -l/--load, -s/--save, -e/--epoch
+#command line switches, supports -l/--load, -s/--save, -e/--epoch, -b/--batchsize, -t/--thread
 parser = OptionParser()
 parser.add_option("-l", "--load", action="store", type="string", dest="inputfile", help="Specify a pretrained model weight db to load")
 parser.add_option("-s", "--save", action="store", type="string", dest="outputfile", help="Specify a model output filename to train the LFW dataset")
 parser.add_option("-e", "--epoch", action="store", type="int", dest="epoch", default=5, help="Number of epochs, default=5")
 parser.add_option("-b", "--batchsize", action="store", type="int", dest="batchsize", default=8, help="Number of batches, default=8")
+parser.add_option("-t", "--thread", action="store", type="int", dest="numworkers", default=4, help="Number of workers to spawn in data collection, set this to number of threads available on CPU. Default=4 (quadcore no HT, eg i5)")
 (options, args) = parser.parse_args()
 inputfilename = options.inputfile
 outputfilename = options.outputfile
 epoch = options.epoch
 batchsize = options.batchsize
+numworkers = options.numworkers
 
 #dataset generator
 class MakeDataset(Dataset):
@@ -44,39 +47,80 @@ class MakeDataset(Dataset):
 			string2 = root_dir + y
 			self.inputimages1.append(string1)
 			self.inputimages2.append(string2)
-			self.weights.append(z)
+			self.weights.append(int(z))
 
-	#returns number of entries in dataset (number of images in inputimages1)
+	#returns number of entries in dataset (number of entries in weights)
 	def __len__(self):
-		return len(self.inputimages1)
+		return len(self.weights)
 
 	def __getitem__(self,idx):
-		image1 = torch.from_numpy(np.asarray(cv2.imread(self.inputimages1[idx], 1)))
-		image2 = torch.from_numpy(np.asarray(cv2.imread(self.inputimages2[idx], 1)))
-		weight = torch.from_numpy(np.asarray(self.weights[idx]).reshape[1])
-		return {'image1': image1, 'image2':image2, 'weight':weight}
+		image1 = Image.open(self.inputimages1[idx])
+		image2 = Image.open(self.inputimages2[idx])
+		image1 = image1.convert('RGB')
+		image2 = image2.convert('RGB')
+		image1 = image1.resize((128, 128), Image.ANTIALIAS)
+		image2 = image1.resize((128, 128), Image.ANTIALIAS)
+		weight = self.weights[idx]
+		if self.transform is not None:
+			image1 = self.transform(image1)
+			image2 = self.transform(image2)
+		return image1, image2, weight
 
+class SiameseNetwork(nn.Module):
+	def __init__(self):
+		super(SiameseNetwork, self).__init__()
+		#steps 1-15
+		self.net = nn.Sequential(
+			nn.Conv2d(3, 64, kernel_size=(5,5), padding=2, stride=(1,1)),
+			nn.ReLU(inplace=True), 
+			nn.BatchNorm2d(64),
+			nn.MaxPool2d((2,2), stride=(2,2)),
+			nn.Conv2d(64, 128, kernel_size=(5,5), padding=2, stride=(1,1)),
+			nn.ReLU(inplace=True),
+			nn.BatchNorm2d(128),
+			nn.MaxPool2d((2,2), stride=(2,2)),
+			nn.Conv2d(128, 256, kernel_size=(3,3), stride=(1,1), padding=1),
+			nn.ReLU(inplace=True),
+			nn.BatchNorm2d(256),
+			nn.MaxPool2d((2,2), stride=(2,2)),
+			nn.Conv2d(256, 512, kernel_size=(3,3), stride=(1,1), padding=1),
+			nn.ReLU(inplace=True),
+			nn.BatchNorm2d(512))
+
+		#steps 17-19
+		self.vec = nn.Sequential(
+			nn.Linear(16*16*512,1024),
+			nn.ReLU(inplace=True),
+			nn.BatchNorm2d(1024))
+	def step(self, x):
+		output = self.net(x)
+		output = output.view(-1, 16*512) #flatten layer, step 16
+		output = self.vec(output)
+		return output
+	def forward(self, input1, input2):
+		output1 = self.step(input1)
+		output2 = self.step(input2)
+		return output1, output2
+# def imshow(img):
+# 	npimg = img.numpy()
+# 	plt.imshow(np.transpose(npimg, (1,2,0)))
+# 	plt.show()
 
 #P1A
 #indicates we want to train a network and save it
 if outputfilename is not None:
 	trans = transforms.Compose([transforms.ToTensor()])
 	trainingset = MakeDataset(txt_file='train.txt', root_dir="./lfw/", transform = trans)
-	trainloader = DataLoader(dataset=trainingset, batch_size=batchsize, num_workers=1)
-	# for image1, image2, label in trainloader:
-	# 	#imagex1 = cv2.imread(image1, 1)
-	# 	#imagex2 = cv2.imread(image2, 1)
-	# 	cv2.imshow('test1', image1)
-	# 	cv2.imshow('test2', image2)
-	# 	cv2.waitKey(0)
-	# 	cv2.destroyAllWindows()
-	img1, img2, weight = next(iter(trainloader))
-	cv2.imshow('test', img1)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	trainloader = DataLoader(dataset=trainingset, batch_size=batchsize, num_workers=numworkers)
+	model = SiameseNetwork().cuda()
+	
+	# iteration = iter(trainloader)
+	# imagex1, imagex2, label = iteration.next()
+	# grid = torchvision.utils.make_grid(imagex1)
+	# imshow(grid)
 
 #indicates we want to load neural weights and run testing
 if inputfilename is not None:
 	print(inputfilename) #debug
 	testset = MakeDataset(txt_file='test.txt', root_dir='./lfw/')
-
+	testloader = DataLoader(dataset=testset, batch_size=batchsize, num_workers=numworkers)
